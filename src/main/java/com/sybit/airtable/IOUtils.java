70a58589aba6c361464abcd16ade328eaef51315
stritti <stritti@users.noreply.github.com>
2017-03-17 15:22:58 +0100

    add modifier

--- src/main/java/com/sybit/airtable/IOUtils.java
+++ src/main/java/com/sybit/airtable/IOUtils.java
@@ -20,7 +20,7 @@ final class IOUtils {
         }
     }
 
-    static String convertStreamToString(java.io.InputStream is) {
+    public static String convertStreamToString(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
     }
