4d7b01d3519a3c74115d007f335a33c7212461e2
fzr <Fabian.zeller@sybib.de>
2017-03-30 10:00:10 +0200

    Implemented Destroy Method with Tests
    Added Test Specific Files, New Class Delete to handle Delete Responses, Implemented Method in Table, Added Attributes to Actor Class

---
+++ src/main/java/com/sybit/airtable/vo/Delete.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sybit.airtable.vo;
+
+/**
+ *
+ * @author fzr
+ */
+public class Delete {
+    
+    private boolean deleted;
+    private String id;
+
+    /**
+     * @return the deleted
+     */
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    /**
+     * @param deleted the deleted to set
+     */
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
+}
